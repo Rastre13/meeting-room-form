79688d540456557a07f953efe7341e2d3354a8ee
Lyrismet <ana0203tir@gmail.com>
2023-05-07 18:24:55 +0700

    feature: added Comment component

---
+++ src/components/EnterComment.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function EnterComment() {
+    return (
+        <div>
+            <label htmlFor="comment"></label>
+            <textarea placeholder='Введите комментарий...'>
+
+            </textarea>
+        </div>
+    );
+}
+
+export default EnterComment;
