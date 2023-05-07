50c30269cb83704d85de7f5c420a892f0f2d3518
Lyrismet <ana0203tir@gmail.com>
2023-05-07 18:25:15 +0700

    feature: added Tower component

---
+++ src/components/SelectTower.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+function SelectTower({ value, onChange }) {
+    return (
+        <div>
+            <label htmlFor="tower">Башня</label>
+            <select
+                id="tower"
+                name="tower"
+                className="form__label"
+                value={value}
+                onChange={onChange}
+            >
+                <option disabled value="">
+                    Выберите башню
+                </option>
+                <option value="A">Башня А</option>
+                <option value="B">Башня Б</option>
+            </select>
+        </div>
+    );
+}
+export default SelectTower;
